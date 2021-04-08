a4d56d2b327da50f8be465eb1060cd6fbc0af9d2
Álvaro Martínez <amartinez@robotnik.es>
2021-04-08 13:28:57 +0200

    Change to safety mode

--- src/front_vertical_laser_driver.py
+++ src/front_vertical_laser_driver.py
@@ -1,25 +1,44 @@
 #!/usr/bin/env python
 import rospy
 from robotnik_msgs.msg import inputs_outputs
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Bool
 
 class VerticalLaser:
-    def pantalla(self, data):
-        print(data)
 
-    def callback(self, data):
-        print("Se recibio' algo\n")
-        rospy.loginfo("Input 2 %b", data.digital_inputs[2])
-        rospy.loginfo("Input 3 %b", data.digital_inputs[3])
+    def callback_hw(self, data):
+        if(data.digital_inputs[self.io_left_laser] == True or data.digital_inputs[self.io_right_laser] == True):
+	    #The robot is not able to move
+            self.stop = True
+        else:
+	    #The robot is able to move
+            self.stop = False
+        
+    def callback_cmd(self, data):
+        if(not self.stop):
+            self.pub.publish(data)
+        else:
+            self.pub.publish(self.cmd_vel_zero)
     
     def __init__(self):
-        rospy.loginfo("Nodo ejecutado correctamente")
-        self.topic = rospy.get_param('~topic', '/rb1_base/robotnik_base_hw/io')
-        self.frame_id = rospy.get_param('~frame_id', 'front_vertical_laser')
+        rospy.loginfo("Front vertical laser initialize")
 
+        self.prefix = rospy.get_param('~prefix', 'robot_')
+        self.io_left_laser = rospy.get_param("~io_left_laser", 3)
+        self.io_right_laser = rospy.get_param("~io_right_laser", 4)
+        self.topic_in = rospy.get_param('~topic_in', '/robot/robotnik_base_hw/io')
+        self.topic_cmd_unsafe = rospy.get_param('~topic_cmd_unsafe', 'move_base/cmd_vel_unsafe')
+        self.topic_cmd_safe = rospy.get_param('~topic_cmd_safe', 'move_base/cmd_vel')
+        self.stop = False
+        self.cmd_vel_zero = Twist()
 
-        rospy.init_node('front_vertical_laser_driver', anonymous=False)
+        rospy.init_node('front_vertical_laser_security', anonymous=False)        
 
-        rospy.Subscriber(self.topic, inputs_outputs, self.callback)
+        rospy.Subscriber(self.topic_in, inputs_outputs, self.callback_hw)
+        rospy.Subscriber(self.topic_cmd_unsafe, Twist, self.callback_cmd)
+
+        self.pub = rospy.Publisher(self.topic_cmd_safe, Twist, queue_size=10)
+
         rospy.spin()
 
 if __name__ == '__main__':
